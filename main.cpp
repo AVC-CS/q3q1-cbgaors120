cb8b0b3f6e11d6819a58efb101ac9e4a53ce4025
cbgaors120 <bgaor@students.avc.edu>
2026-03-13 15:37:08 -0700

    Committing before compiling because my code never runs correctly the first time.

--- main.cpp
+++ main.cpp
@@ -6,17 +6,41 @@ using namespace std;
 int main()
 {
     double a, b, c;
+    double resultpos;
+    double resultneg;
+    double x;
+    double y;
+    double z;
+    
+    
     // TODO
+    
+    x = -b;
+    y = (b * b) - 4 * a * c;
+    z = 2 * a * c;
+    
+    
+    
+    // TODO
+    // TODO
+    // TODO
+    cout << "Enter the integers for your quadratic equation.\n" << "Enter A value." << endl;
+    cin >> a;
+    cout << "Enter B value.\n";
+    cin >> b;
+    cout << "Lastly enter C value.\n";
+    cin >> c;
 
-    // TODO
-    // TODO
-    // TODO
-
+    resultpos = ((x + sqrt(y)) / z);
+    resultneg = ((x - sqrt(y)) / z);
+    
+    
     // TODO
     // TODO
 
     cout << setprecision(2) << fixed;
     // TODO
+    cout << "When adding square root: " << resultpos << ". When subtracting square root: " << resultneg << endl;
 
     return 0;
 }
